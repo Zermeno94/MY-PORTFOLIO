716521d6d6e110ac234179910541637856315a1b
MIRANDA <miranda.zermeno4@gmail.com>
2021-09-30 23:50:40 -0500

    attempting to add splash to beging of portfolio

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+// work in progres.
+//trying to create a splash to portfolio 
+
+//let intro = document.querySelector('.intro');
+//let logo= document.querySelector('.logo-header');
+//let logo= document.querySelector('.miranda-logo');
+
+//window.addEventListener('DOMContentLoaded', ()=>{
+
+    //setTimeout(()=>{
+        //logoSpan.forEach((span,idx) => {
+            //setTimeout((){
+                //span.classList.add('active');
+            //}, (idx + 1) * 400)
+        // });
+        // setTimeout(()=>{
+            // logoSpan.forEach((span,idx)=>{
+                // setTimeout(()=>{
+                    // span.classList.remove('active');
+                    // span.clasList.add('fade');
+                // },)
+            // })
+        // })
+// })
